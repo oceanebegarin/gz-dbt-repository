003da551f32481f392575052783255751a944a00
oceane.begarin@gmail.com <oceane.begarin@gmail.com>
2025-11-05 10:33:32 +0000

    test

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
 SELECT
-    s.products_id,
-    s.date_date,
-    s.orders_id,
-    s.revenue,
-    s.quantity,
-    p.purchase_price,
-    ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
-    ROUND(s.revenue - (s.quantity * p.purchase_price), 2) AS margin
-FROM {{ ref('stg_raw_sales') }} AS s
-LEFT JOIN {{ ref('stg_raw_product') }} AS p
-    USING (products_id)
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
